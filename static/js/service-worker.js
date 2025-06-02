9e294652b71beb93f383347629822f258858ef60
DJObleezy <156115770+Djobleezy@users.noreply.github.com>
2025-06-01 20:27:05 -0700

    Fix asset paths and cache vendor scripts (#290)
    - correct static asset links by using url_for
    - cache vendor scripts in service-worker.js for offline use
    - update service worker registration path

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -19,6 +19,9 @@ const ASSETS_TO_CACHE = [
     '/static/js/batchFetch.js',
     '/static/js/audio.js',
     '/static/js/easterEgg.js',
+    '/static/vendor/jquery-3.7.0.min.js',
+    '/static/vendor/chart.min.js',
+    '/static/js/chartjs-plugin-annotation-lite.js',
     '/static/js/service-worker.js',
     'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&family=VT323&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
